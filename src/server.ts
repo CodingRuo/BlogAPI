66233989a4b0e92593364342cb13d079e5862ca3
René Schober <r.schober@outlook.com>
2025-06-04 22:00:50 +0200

    feat: aktualisiere Serverkonfiguration und füge neue Abhängigkeiten hinzu
    - Aktualisiert nodemon.json, um tsconfig-paths zu verwenden.
    - Hinzugefügt tsconfig-paths als Abhängigkeit in package.json und pnpm-lock.yaml.
    - Konfiguriert tsconfig.json mit Pfaden für die Modulauflösung.
    - Implementiert eine einfache Route in server.ts, die eine JSON-Antwort zurückgibt.

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,16 @@
  * @license Apache-2.0
  */
 
+import config from '@/config';
 import express from 'express';
-import config from './config';
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on http://localhost:${config.PORT}`);
