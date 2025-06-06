6503f77a11467627e1a3c18e25840dadd96bb1b1
René Schober <r.schober@outlook.com>
2025-06-06 07:21:43 +0200

    feat: erweitere Benutzerregistrierung mit zusätzlicher Validierung und Rollenspezifikation
    - Füge Validierung für die E-Mail-Adresse hinzu, um sicherzustellen, dass der Benutzer nicht bereits existiert.
    - Implementiere Validierungsregeln für das Passwort, um eine Mindestlänge von 8 Zeichen zu verlangen.
    - Ergänze die Möglichkeit, eine Rolle anzugeben, und beschränke diese auf 'admin' oder 'user'.

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -5,6 +5,7 @@
 
 import { register } from '@/controllers/v1/auth/register';
 import { validationError } from '@/middlewares/validationError';
+import { User } from '@/models/user';
 import { Router } from 'express';
 import { body } from 'express-validator';
 
@@ -18,7 +19,24 @@ authRouter.post('/register',
         .isLength({ max: 50 })
         .withMessage('Email must be less than 50 characters')
         .isEmail()
-        .withMessage('Invalid email address'),
+        .withMessage('Invalid email address')
+        .custom(async (value) => {
+            const userExists = await User.exists({ email: value });
+            if (userExists) {
+                throw new Error('User email or password is invalid')
+            }
+        }),
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long'),
+    body('role')
+        .optional()
+        .isString()
+        .withMessage('Role must be a string')
+        .isIn(['admin', 'user'])
+        .withMessage('Role must be either admin or user'),
     validationError,
     register
 );
